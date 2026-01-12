9d5f71a50c5bd17610a532b99f686811859067f8
reiworkout-droid <rei.workout@gmail.com>
2026-01-12 22:21:31 +0900

    DBに保存

--- create.php
+++ create.php
@@ -3,43 +3,54 @@
 include('functions.php');
 
 if (
-  !isset($_POST['menu']) || $_POST['menu'] === '' ||
-  !isset($_POST['weight']) || $_POST['weight'] === '' ||
-  !isset($_POST['reps']) || $_POST['reps'] === '' ||
-  !isset($_POST['memo']) || $_POST['memo'] === '' ||
-  !isset($_POST['max']) || $_POST['max'] === '' ||
-  !isset($_POST['date']) || $_POST['date'] === ''
+    empty($_POST['categories']) ||
+    empty($_POST['menu']) ||
+    empty($_POST['weight']) ||
+    empty($_POST['reps']) ||
+    empty($_POST['max']) ||
+    empty($_POST['memo']) ||
+    empty($_POST['date'])
 ) {
-  exit('paramError');
+    exit('paramError');
 }
 
 $categories = $_POST['categories'];
 $menu = $_POST['menu'];
 $weight = $_POST['weight'];
 $reps = $_POST['reps'];
-$memo = $_POST['memo'];
 $max = $_POST['max'];
+$memo = $_POST['memo'];
 $date = $_POST['date'];
 
 // DB接続
 // $pdo = connect_to_db();//さくら用
 $pdo = connect_to_db_pre();//ローカルホスト用
 
-$sql = 'INSERT INTO workout_menu(id, menu, weight, reps, date,created_at, updated_at) VALUES(NULL, :menu, :weight, reps, date, now(), now())';
+$sql = 'INSERT INTO workout_memo(id, categories, menu, weight, reps, max, memo, date, created_at, updated_at) VALUES(NULL, :categories, :menu, :weight, :reps, :max, :memo, :date, now(), now())';
 
 $stmt = $pdo->prepare($sql);
-$stmt->bindValue(':menu', $menu, PDO::PARAM_STR);
-$stmt->bindValue(':weight', $weight, PDO::PARAM_STR);
-$stmt->bindValue(':reps', $reps, PDO::PARAM_STR);
-$stmt->bindValue(':memo', $memo, PDO::PARAM_STR);
-$stmt->bindValue(':max', $max, PDO::PARAM_STR);
-$stmt->bindValue(':date', $date, PDO::PARAM_STR);
 
-try {
-  $status = $stmt->execute();
-} catch (PDOException $e) {
-  echo json_encode(["sql error" => "{$e->getMessage()}"]);
-  exit();
+for ($i = 0; $i < count($weight); $i++) {
+
+    // 空行スキップ（超重要）
+    if ($weight[$i] === '' || $reps[$i] === '') {
+        continue;
+    }
+
+    $stmt->bindValue(':categories', $categories[$i], PDO::PARAM_STR);
+    $stmt->bindValue(':menu', $menu[$i], PDO::PARAM_STR);
+    $stmt->bindValue(':weight', $weight[$i], PDO::PARAM_INT);
+    $stmt->bindValue(':reps', $reps[$i], PDO::PARAM_INT);
+    $stmt->bindValue(':max', $max[$i], PDO::PARAM_STR);
+    $stmt->bindValue(':memo', $memo[$i], PDO::PARAM_STR);
+    $stmt->bindValue(':date', $date, PDO::PARAM_STR);
+
+    try {
+    $status = $stmt->execute();
+    } catch (PDOException $e) {
+    echo json_encode(["sql error" => "{$e->getMessage()}"]);
+    exit();
+    }
 }
 
 header("Location:index.php");
